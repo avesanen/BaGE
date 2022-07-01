3be2d090c47ffc6a32c3c9df15fc45b230bdce39
Antti Vesanen <avesanen@protonmail.com>
2022-07-01 11:23:27 +0300

    work work

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
-#include <iostream>
 #include "Engine.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
-    // Get Engine singleton
     Engine *engine = &Engine::getInstance();
-
     engine->Run("snek.lua");
     return 0;
 }
